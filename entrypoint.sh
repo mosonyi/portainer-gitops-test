ef1d7e4b7fa679aa96f4358b5cd4c7f1d4fe6814
Zoltan Mosonyi <mosonyi@esix.hu>
2024-01-21 18:24:16 +0100

    Added wait time

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,9 @@ SUBJ=$NODENAME
 MESSAGE="Node restarted"
 CHARSET="utf-8"
 
+echo "Wait 10 sec before sending the email"
+sleep 10
+
 sendemail -f $FROM -t $TO_EMAIL -u $SUBJ -s $SERVER -m $MESSAGE -v -o message-charset=$CHARSET
 
 echo "Restarted at: "
